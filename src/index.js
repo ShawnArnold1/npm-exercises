b20798aadc7be091ed0e6f7d9b4e4cd3184dcb50
ShawnArnold1 <shawnthearnold@gmail.com>
2018-12-20 08:57:21 -0600

    Uploaded npm exercise

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const $ = require('jquery');
+const sayHello = name => console.log('Hello!');
+
+sayHello();
+
+alert('hey')
+
+$('#npmHead').click(e => alert('clicked!'))
+
+$('body').html('<h1><em>Fingers Crossed!</em></h1>');
+
+alert('end of script');
